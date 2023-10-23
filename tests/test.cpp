224882cd1f6fdca6619fd8cf6ba71c3c1c68c1d0
juanhosz <jhoszowski@fi.uba.ar>
2023-10-23 15:23:21 -0300

    Se agrega suite de Google tests para testear sockets

---
+++ tests/test.cpp
@@ -0,0 +1,14 @@
+#include "gtest/gtest.h"
+
+TEST(sample_test_case, sample_test)
+{
+    EXPECT_EQ(5, 5);
+}
+
+TEST(sample_test_case, smple_case){
+    EXPECT_EQ(4,4);
+}
+
+TEST(sample_test_case, Simple){
+    EXPECT_EQ(2,2);
+}

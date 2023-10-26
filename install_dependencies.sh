7d699458a3838e2d3cf56eef0d5c1c96d9c18989
IVetrano <ivetrano@fi.uba.ar>
2023-10-26 19:11:50 -0300

    Se agregaron las dependencias de Qt

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,4 +5,4 @@ sudo apt-get update
 sudo apt-get upgrade
 
 # Instala las dependencias requeridas
-sudo apt-get install cmake libyaml-cpp-dev libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsdl2-mixer-dev
+sudo apt-get install cmake libyaml-cpp-dev libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsdl2-mixer-dev qt5-default

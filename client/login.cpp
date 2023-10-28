bfd50d308e5bd44407e7063a774c897ec2403e79
Ramiro <rrecchia@fi.uba.ar>
2023-10-28 12:27:56 -0300

    Patanlla de inicio actualizada. Se incluyeron campos para el servidor y el puerto y un boton para comenzar el juego. Es mandatorio colocar datos sobre los text box pero pueden ser cualquier cosa

--- client/login.cpp
+++ client/login.cpp
@@ -1,6 +1,6 @@
 #include <QApplication>
 
-#include "menu.h"
+#include "mainwindow.h"
 #include "login.h"
 
 Login::Login(int argc, char** argv) : app(argc, argv) {}
@@ -8,7 +8,7 @@ Login::Login(int argc, char** argv) : app(argc, argv) {}
 Login::~Login() {}
 
 int Login::start() {
-    Menu menu;
+    MainWindow menu;
     menu.show();
     return this->app.exec();
 }
---
+++ client/mainwindow.cpp
@@ -0,0 +1,30 @@
+#include <QMessageBox>
+
+#include "mainwindow.h"
+#include "ui/ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(ui->start, SIGNAL(clicked()), this, SLOT(onComenzarButtonClicked()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::onComenzarButtonClicked() {
+    QString serverText = ui->serverInput->text();
+    QString portText = ui->portInput->text();
+
+    // Validar los datos como lo desees, por ejemplo, verificar que no estén vacíos.
+    if (serverText.isEmpty() || portText.isEmpty()) {
+        QMessageBox::warning(this, "Error", "Por favor, complete ambos campos.");
+        return;
+    }
+
+    MainWindow::close();
+}

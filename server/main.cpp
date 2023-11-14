eedce9a1870f4e868a0d40c0a988c48c2065ebb5
Ramiro <rrecchia@fi.uba.ar>
2023-11-14 17:29:57 -0300

    port update

--- server/main.cpp
+++ server/main.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char* argv[]) {
 
-    const char* name = "1576";
+    const char* name = "1577";
     BroadCaster broadcaster;
     Queue<std::shared_ptr<Comando>> acciones_a_realizar;
     Lobby lobby;

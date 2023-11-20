1bc9d521fd4299cd7df0b00133a5c65ed7cfef1a
RamiroRecchia <rrecchia@fi.uba.ar>
2023-11-20 10:45:14 -0300

    El gusano puede saltar hacia adelante y atras

--- server/comandos/saltar.cpp
+++ server/comandos/saltar.cpp
@@ -8,4 +8,9 @@ Saltar::Saltar(uint8_t dir,uint32_t id):Comando(id),direc_salto(dir){
 
 void Saltar::realizar_accion(Mapa* mapa , uint32_t id){
 
+    if (direc_salto == 0x01) {
+        mapa->JumpWormForward(id);
+    } else {
+        mapa->JumpWormBackward(id);
+    }
 }

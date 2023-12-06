ef7c08cfb0a7e7a684dd6a8c83f5f6c1459ffbb6
juanhosz <jhoszowski@fi.uba.ar>
2023-12-05 21:01:17 -0300

    Se cambia el .gitmodules y se agrega install_dependencies para que descargeu yaml

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,3 +6,4 @@ sudo apt-get upgrade
 
 # Instala las dependencias requeridas
 sudo apt-get install cmake libyaml-cpp-dev libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsdl2-mixer-dev qt5-default
+git submodule update --init --recursive
